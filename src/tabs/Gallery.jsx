50d234fb623191eebd99b745d424890bc6024523
MaryShtayn <106402872+MaryShtayn@users.noreply.github.com>
2023-03-11 11:29:49 +0100

    added loader

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+import { Loader } from 'components/Loader/Loader';
 
 export class Gallery extends Component {
   state = {
@@ -9,44 +10,66 @@ export class Gallery extends Component {
     page: 1,
     photos: [],
     showBtn: false,
+    isEmpty: false,
+    isLoading: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
     const { search, page } = this.state;
     if (prevState.search !== search || prevState.page !== page) {
-      ImageService.getImages(search, page).then(({photos, total_results})=> {
-        this.setState((prevState)=>({photos: [...prevState.photos, ...photos], showBtn: page < Math.ceil(total_results/15)}))
-      });
+      this.setState({ isLoading: true });
+      ImageService.getImages(search, page)
+        .then(({ photos, total_results }) => {
+          if (!photos.length) {
+            this.setState({ isEmpty: true });
+            return;
+          }
+          this.setState(prevState => ({
+            photos: [...prevState.photos, ...photos],
+            showBtn: page < Math.ceil(total_results / 15),
+          }));
+        })
+        .catch(error => console.log('Error'))
+        .finally(() => this.setState({ isLoading: false }));
     }
   }
 
   onSubmit = search => {
-    console.log(search);
-    this.setState({ search });
+    this.setState({
+      search,
+      page: 1,
+      photos: [],
+      showBtn: false,
+      isEmpty: false,
+    });
   };
 
-  onClickBtn = ()=>{
-    this.setState((prevState)=> ({page: prevState.page + 1}));
-  }
+  onClickBtn = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
 
   render() {
     return (
       <>
         <SearchForm onSubmit={this.onSubmit} />
         <Grid>
-          {
-            this.state.photos.map(photo => (
-              <GridItem key={photo.id}>
-                <CardItem color={photo.avg_color}>
-                  <img src={photo.src.large} alt={photo.alt} />
-                </CardItem>
-              </GridItem>
-            ))
-          }
+          {this.state.photos.map(photo => (
+            <GridItem key={photo.id}>
+              <CardItem color={photo.avg_color}>
+                <img src={photo.src.large} alt={photo.alt} />
+              </CardItem>
+            </GridItem>
+          ))}
         </Grid>
 
-        {this.state.showBtn && <Button onClick={this.onClickBtn}>Load more</Button>}
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        {this.state.showBtn && (
+          <Button onClick={this.onClickBtn}>Load more</Button>
+        )}
+        {this.state.isEmpty && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
+
+        {this.state.isLoading && <Loader />}
       </>
     );
   }

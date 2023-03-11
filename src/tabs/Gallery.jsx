515cfd19146ab4b911233fcbd7b99dffa0df346b
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-03-11 11:52:04 +0200

    show Gallery

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -7,12 +7,16 @@ export class Gallery extends Component {
   state = {
     search: '',
     page: 1,
+    photos: [],
+    showBtn: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
     const { search, page } = this.state;
-    if (prevState.search !== search) {
-      ImageService.getImages(search, page).then(data => console.log(data));
+    if (prevState.search !== search || prevState.page !== page) {
+      ImageService.getImages(search, page).then(({photos, total_results})=> {
+        this.setState((prevState)=>({photos: [...prevState.photos, ...photos], showBtn: page < Math.ceil(total_results/15)}))
+      });
     }
   }
 
@@ -21,10 +25,27 @@ export class Gallery extends Component {
     this.setState({ search });
   };
 
+  onClickBtn = ()=>{
+    this.setState((prevState)=> ({page: prevState.page + 1}));
+  }
+
   render() {
     return (
       <>
         <SearchForm onSubmit={this.onSubmit} />
+        <Grid>
+          {
+            this.state.photos.map(photo => (
+              <GridItem key={photo.id}>
+                <CardItem color={photo.avg_color}>
+                  <img src={photo.src.large} alt={photo.alt} />
+                </CardItem>
+              </GridItem>
+            ))
+          }
+        </Grid>
+
+        {this.state.showBtn && <Button onClick={this.onClickBtn}>Load more</Button>}
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
